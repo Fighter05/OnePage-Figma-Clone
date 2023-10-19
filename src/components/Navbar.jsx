73b58827a54fcac2c4ecf177c92c720e7ce7b9f7
fighter05 <sitthipon.sint@gmail.com>
2023-10-19 14:50:11 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="border border-red-500 h-[80px]">
+      <p>Navbar</p>
+    </div>
+  );
+};
+
+export default Navbar;
